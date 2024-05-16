a504ec9e6391ba39b0282270f11e38136ad309ca
Yanko Rashkov <thewhitedead@abv.bg>
2024-05-16 14:25:19 +0300

    Refator project|

--- Stoyanoff_webpage/website/apps.py
+++ Stoyanoff_webpage/website/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stoyanoff_guitars.website'
+    name = 'Stoyanoff_webpage.website'
