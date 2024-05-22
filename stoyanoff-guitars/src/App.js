8b2011168804abd19c060524495b10196d9069b7
Peter Stoyanov <peterstoyanov83@gmail.com>
2024-05-22 15:24:13 +0300

    ..

--- stoyanoff-guitars/src/App.js
+++ stoyanoff-guitars/src/App.js
@@ -15,7 +15,7 @@ import Guitar2 from './components/guitar2';
 import Guitar3 from './components/guitar3';
 import Guitar4 from './components/guitar4';
 import Guitar5 from './components/guitar5';
-import Logo from './components/logo';
+
 import './App.css';
 
 
